908fb6fba25ab5b9c0a4bd5774c641acbf560a3a
Admin <nikita.ishmekeev.2003@gmail.com>
2024-09-03 21:35:49 +0500

    Добавьте файлы проекта.

---
+++ pr4/Point.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace pr4
+{
+    public class Point
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public Point(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Deconstruct(out float x, out float y)
+        {
+            x = X;
+            y = Y;
+        }
+    }
+}
---
+++ pr4/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace pr4 {
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Point point = new Point(-10F, -10.5F);
+            IsInSquare(point);
+        }
+
+        public static void IsInSquare(Point point)
+        {
+            var (x, y) = point;
+            if (x * x + y * y > 25*25 || y > 0 || (y > -Math.Abs(x) && x != 0))
+            {
+                Console.WriteLine("НЕТ");
+            }
+            else if (y < -Math.Abs(x))
+            {
+                Console.WriteLine("ДА");
+            }
+            else
+            {
+                Console.WriteLine("На границе");
+            }
+        }
+    }
+}
