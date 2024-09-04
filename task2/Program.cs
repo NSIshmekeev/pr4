7af10934b78031cc87f6512287585cc92f4e1958
Admin <nikita.ishmekeev.2003@gmail.com>
2024-09-04 12:20:36 +0500

    fix

---
+++ task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Запрос количества месяцев, прошедших с 1 января 1990 года
+        Console.WriteLine("Введите количество месяцев, прошедших с 1 января 1990 года:");
+        int monthsPassed = int.Parse(Console.ReadLine());
+
+        // Массив с названиями месяцев
+        string[] months = {
+            "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
+            "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"
+        };
+
+        // Определение текущего месяца
+        int currentMonthIndex = (monthsPassed % 12);
+        string currentMonth = months[currentMonthIndex];
+
+        // Вывод названия текущего месяца
+        Console.WriteLine($"Текущий месяц: {currentMonth}");
+    }
+}
